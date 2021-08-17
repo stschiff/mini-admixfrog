903f623fb26fe040da86de103dbd319dea1b5c7b
Stephan Schiffels <stephan_schiffels@mac.com>
2021-08-17 17:44:41 +0200

    initial commit

---
+++ defs.R
@@ -0,0 +1,81 @@
+emission_prob <- function(genotype, state_tuple, source_a, source_d, a_prime=0.1, d_prime=0.1) {
+  # Homozygous state
+  if(state_tuple[1] == state_tuple[2]) {
+    k <- state_tuple[1]
+    a <- source_a[k]
+    d <- source_d[k]
+    numerator <- beta(genotype + d + d_prime, 2 - genotype + a + a_prime)
+    denominator <- beta(d + d_prime, a + a_prime)
+    binomial_coefficient <- if(genotype == 1) 2
+                            else if(genotype == 0 || genotype == 2) 1
+                            else stop(paste0("unknown genotype value ", genotype))
+    return(binomial_coefficient * numerator / denominator)
+  }
+  # Heterozygous state
+  else {
+    k1 <- state_tuple[1]
+    k2 <- state_tuple[2]
+    a1 <- source_a[k1]
+    a2 <- source_a[k2]
+    d1 <- source_d[k1]
+    d2 <- source_d[k2]
+    denominator <- (d1 + d_prime + a1 + a_prime) * (d2 + d_prime + a2 + a_prime)
+    if(genotype == 0) {
+      numerator <- (a1 + a_prime) * (a2 + a_prime)
+    }
+    else if(genotype == 1) {
+      numerator <- (a1 + a_prime) * (d2 + d_prime) + (a2 + a_prime) * (d1 + d_prime)
+    }
+    else if(genotype == 2) {
+      numerator <- (d1 + d_prime) * (d2 + d_prime)
+    } else {
+      stop(paste0("unkown genotype value ", genotype))
+    }
+    return(numerator / denominator)
+  }
+}
+
+# Expect data to be a data-frame with the following column-names:
+# chrom
+# pos
+# genotype
+# source_a (list column)
+# source_d (list column)
+
+# Currently this is hard-coded for 2 sources.
+run_forward <- function(data, equilibrium_probs, transition_matrix, a_prime, d_prime) {
+  state_tuples <- list(c(1, 1), c(1, 2), c(2, 2))
+  L <- nrow(data)
+  forward_vec <- matrix(0.0, ncol=L, nrow=3)
+  scaling_factors <- rep(1.0, L)
+  for(i in 1:L) {
+    g <- data$genotype[i]
+    a <- data$source_a[[i]]
+    d <- data$source_d[[i]]
+    emission_vec <- purrr::map(state_tuples, function(s) {emission_prob(g, s, a, d)})
+    previous_forward_vec <- if(i == 1) equilibrium_probs else forward_vec[,i-1]
+    new_forward_vec <- (transition_matrix %*% previous_forward_vec) * emission_vec
+    scaling_factors[i] <- sum(new_forward_vec)
+    forward_vec[,i] <- new_forward_vec / scaling_factors[i]
+  }
+  return(list(forward_vec, scaling_factors))
+}
+
+run_backward <- function(data, transition_matrix, a_prime, d_prime) {
+  state_tuples <- list(c(1, 1), c(1, 2), c(2, 2))
+  L <- nrow(data)
+  forward_vec <- matrix(0.0, ncol=L, nrow=3)
+  scaling_factors <- rep(1.0, L)
+  for(i in 1:L) {
+    g <- data$genotype[i]
+    a <- data$source_a[[i]]
+    d <- data$source_d[[i]]
+    emission_vec <- purrr::map(state_tuples, function(s) {emission_prob(g, s, a, d)})
+    previous_forward_vec <- if(i == 1) equilibrium_probs else forward_vec[,i-1]
+    new_forward_vec <- (transition_matrix %*% previous_forward_vec) * emission_vec
+    scaling_factors[i] <- sum(new_forward_vec)
+    forward_vec[,i] <- new_forward_vec / scaling_factors[i]
+  }
+  return(list(forward_vec, scaling_factors))
+}
+
---
+++ playground.R
@@ -0,0 +1,3 @@
+source("defs.R")
+
+emission_prob(genotype = 0, state_tuple = c(1, 2), source_a = c(0, 2), source_d = c(2, 0))
