8c8ee892a5e964dd8836bcb67db70025d20d2975
Stephan Schiffels <stephan_schiffels@mac.com>
2021-08-31 17:24:13 +0200

    added forward-backward to writeup

--- playground.R
+++ playground.R
@@ -3,8 +3,6 @@ library(ggplot2)
 
 source("defs.R")
 
-emission_prob(genotype = 0, state_tuple = c(1, 2), source_a = c(0, 2), source_d = c(2, 0))
-
 # I have prepared a test file with the first 10,000 SNPs of the input file
 dat <- readr::read_tsv("~/Data/merge_vcf.iceman_obs.wgs_short.tsv") %>%
   dplyr::transmute(
@@ -15,17 +13,14 @@ dat <- readr::read_tsv("~/Data/merge_vcf.iceman_obs.wgs_short.tsv") %>%
     source_d = purrr::map2(s1_dr, s2_dr, c)
   )
 
-s1_prop = 0.97
-s2_prop = 1 - s1_prop
-
 # three states in Hardy-Weinberg eq.
-eq_probs <- c(s1_prop ^ 2, 2 * s1_prop * s2_prop, s2_prop ^ 2)
+eq_probs <- equilibrium_probs(0.03)
 
 # transition probabilities from state i to state j are conditional on state i,
 # so normalised across the second index
-trans_matrix <- matrix(c(0.99, 0.01, 0.01, 0.01, 0.99, 0.01, 0.01, 0.01, 0.99), ncol=3)
+trans_matrix <- transition_probs(0.03, 1000)
 
-fwd_dat <- run_forward(dat, eq_probs, trans_matrix, 0.1, 0.1)
+fwd_dat <- run_forward(dat, trans_matrix, 0.1, 0.1)
 fwd_vec <- fwd_dat[[1]]
 scaling_facs <- fwd_dat[[2]]
 bwd_vec <- run_backward(dat, scaling_facs, eq_probs, trans_matrix, 0.1, 0.1)
@@ -45,3 +40,6 @@ for_plotting <- dat %>%
 
 for_plotting %>%
   ggplot() + geom_area(aes(x = pos, y = prob)) + facet_grid(rows = vars(State))
+
+for_plotting %>%
+  ggplot() + geom_area(aes(x = pos, y = prob, fill = State))
